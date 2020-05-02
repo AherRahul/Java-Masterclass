8a48f5c2e3e1d1d0b93bfbc7c0cd29a3d44013ec
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-05-02 12:57:47 +0530

    Created New program for Print and declacre first variable

---
+++ Declare&PrintFirstVariable/src/PrintAndDeclareVariable.java
@@ -0,0 +1,9 @@
+public class PrintAndDeclareVariable {
+    public static void main (String[] args) {
+        System.out.println("Print Out the first variable...!!!");
+
+        int myFirstVariable = 5;
+
+        System.out.println("value of myFirstVariable is: " + myFirstVariable);
+    }
+}
