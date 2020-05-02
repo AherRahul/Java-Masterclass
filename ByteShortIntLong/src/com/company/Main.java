49c336b014502c757252f5e83ee2b4f9f9b26f8f
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-05-02 18:41:59 +0530

    learning Data types

---
+++ ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //int, byte, short, float, double, long, string, char are the data-types available in Java
+        
+    }
+}
--- PlayWithVariable/src/PlayWithVariable.java
+++ PlayWithVariable/src/PlayWithVariable.java
@@ -2,6 +2,8 @@ public class PlayWithVariable {
     public static void main (String[] args) {
         System.out.println("Let's play with variable's...!!");
 
+        //int, byte, short, float, double, long, string, char are the data-types available in Java
+
         //Declaring two variables varA and varB and print out them..
         int varA = 5;
         int varB = 10;
@@ -16,14 +18,21 @@ public class PlayWithVariable {
         int inLineAdd = 20 + 19;
         System.out.println("Addition of inline add: " + inLineAdd);
 
+        int expressionVariable = (20 + 2) + (10 * 5);
+        System.out.println("Value of expression variable = " + expressionVariable);
+
         //Defining the string variable
-        String firstString = "Hello Wolrd 1";
+        String firstString = "Hello World 1";
         System.out.println("Print the first string " + firstString);
 
         //Define the two string variable and concatenate them
         String strA = "String";
         String strB = "Concatenate Demo";
-
         System.out.println(strA + " " + strB);
+
+        //
+        int myLastValue;
+        myLastValue = 1000 - varC;
+        System.out.println(myLastValue);
     }
 }
