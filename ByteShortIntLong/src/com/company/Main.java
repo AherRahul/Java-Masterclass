ddd3f0d0963333072462062af9e6def42eec7161
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-05-02 18:49:53 +0530

    Overflow and underflow concept

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         //int, byte, short, float, double, long, string, char are the data-types available in Java
-        
+        int myValues = 10000;
+
+        //Integer Is Wrapper Class for int data type
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        //Min Value = -2147483648 and Max value = 2147483647
+        System.out.println("Min Value = " + myMinIntValue + " and Max value = " + myMaxIntValue);
+
+        //Busted max value = -2147483648 ===> OverFlow
+        System.out.println("Busted max value = " + (myMaxIntValue + 1));
+
+        //Busted min value= 2147483647 ===> UnderFlow
+        System.out.println("Busted min value= " + (myMinIntValue - 1));
     }
 }
