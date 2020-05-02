e445696e21fce2aeafac32bed0ae42a91004323d
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-05-02 14:04:37 +0530

    Vriable demo added

---
+++ PlayWithVariable/src/PlayWithVariable.java
@@ -0,0 +1,29 @@
+public class PlayWithVariable {
+    public static void main (String[] args) {
+        System.out.println("Let's play with variable's...!!");
+
+        //Declaring two variables varA and varB and print out them..
+        int varA = 5;
+        int varB = 10;
+        System.out.println("value of A = " + varA + " and value of B = " + varB);
+
+
+        //Add two variables and store in third variable and print third variable
+        int varC = varA + varB;
+        System.out.println("Addition of variable A and B = " + varC);
+
+        //Addition of two number while declaring the variable
+        int inLineAdd = 20 + 19;
+        System.out.println("Addition of inline add: " + inLineAdd);
+
+        //Defining the string variable
+        String firstString = "Hello Wolrd 1";
+        System.out.println("Print the first string " + firstString);
+
+        //Define the two string variable and concatenate them
+        String strA = "String";
+        String strB = "Concatenate Demo";
+
+        System.out.println(strA + " " + strB);
+    }
+}
